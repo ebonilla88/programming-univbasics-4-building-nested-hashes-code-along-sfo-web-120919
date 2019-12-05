4f7d6f6f4215c31a92d825396b0d44c22beb2204
Eduardo Bonilla <ebonilla88@gmail.com>
2019-12-05 21:42:44 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,19 +1,19 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {
-       :patriarch => {
-         :name => "Lord Montague", 
-         :age => "53"
-     },
-      :matriarch => {
-          :name => "Lady Montague",
-          :age => "54"
-     },
-      :hero => {
-        :name => "Romeo",
-        :age => "15"
-      },
-      :hero_friends => [
+  :montague => {
+    :patriarch => {
+      :name => "Lord Montague",
+      :age => "53"
+  },
+    :matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+  },
+    :hero => {
+      :name => "Romeo",
+      :age => "15"
+  },
+    :hero_friends => [
       {
          :name => "Benvolio",
          :age => "17",
@@ -26,7 +26,7 @@ def first_challenge
       }
     ]
   },
-    :capulet => {
+  :capulet => {
     :patriarch => {
       :name => "Lord Capulet",
       :age => "50"
